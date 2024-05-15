bcdde0581ba094f4e930e0e69daec649b1db82e5
Serhii Pypa <jupiters797@gmail.com>
2024-05-15 15:06:10 +0300

    Добавьте файлы проекта.

---
+++ 12.1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1
+{
+    abstract class Shape
+    {
+        public abstract double Area();
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public override double Area()
+        {
+            return width * height;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double s1, double s2, double s3)
+        {
+            side1 = s1;
+            side2 = s2;
+            side3 = s3;
+        }
+
+        public override double Area()
+        {
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding  = Encoding.Unicode;
+
+            Circle circle = new Circle(5);
+            Console.WriteLine($"Площа круга: {circle.Area()}");
+
+            Rectangle rectangle = new Rectangle(4, 6);
+            Console.WriteLine($"Площа прямокутника: {rectangle.Area()}");
+
+            Triangle triangle = new Triangle(3, 4, 5);
+            Console.WriteLine($"Площа трикутника: {triangle.Area()}");
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 12.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2
+{
+    class Shape
+    {
+        public string Color { get; set; }
+        public string Type { get; set; }
+
+        public Shape(string color, string type)
+        {
+            Color = color;
+            Type = type;
+        }
+    }
+
+    class Square : Shape
+    {
+        public double SideLength { get; set; }
+
+        public Square(string color, string type, double sideLength) : base(color, type)
+        {
+            SideLength = sideLength;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Square square = new Square("Синій", "Квадрат", 5);
+
+            Console.WriteLine($"Колір: {square.Color}");
+            Console.WriteLine($"Тип: {square.Type}");
+            Console.WriteLine($"Довжина сторони: {square.SideLength}");
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
